c51c0ab732a15dbc76bacf75bdaa95fee5e40997
Thierry Gonard <goth@octo.com>
2021-12-13 12:37:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Advent of Code 2021
+
+Resolutions are written in python3
+
+Don't forget to:
+
+```sh
+pip install -r requirements.txt
+```
+
+To initialize a new day, use the `init.py` script as so:
+
+```sh
+./init.sh {day_number}
+```
---
+++ init.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+import os
+import shutil
+import sys
+
+import requests
+
+
+def create_directory_for_day(directory: str):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def download_input(day: str):
+    url = f"https://adventofcode.com/2021/day/{day}/input"
+    response = requests.get(url)
+    with open(f"day{day}/input.txt", "w") as file:
+        file.write(response.text)
+
+
+def copy_template(directory: str):
+    if not os.path.exists(f"{directory}/main.py"):
+        shutil.copyfile("main.py.template", f"{directory}/main.py")
+
+
+def main(day: str):
+    directory = f"day{day}"
+    create_directory_for_day(directory)
+    # download_input(day)
+    copy_template(directory)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    main(args[1])
