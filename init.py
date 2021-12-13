653f32c687e7d6cc994f62514c2d387471a0220e
Thierry Gonard <goth@octo.com>
2021-12-13 17:08:20 +0100

    Download input of the day

--- init.py
+++ init.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 
 import requests
+from dotenv import load_dotenv
 
 
 def create_directory_for_day(directory: str):
@@ -13,7 +14,9 @@ def create_directory_for_day(directory: str):
 
 def download_input(day: str):
     url = f"https://adventofcode.com/2021/day/{day}/input"
-    response = requests.get(url)
+    response = requests.get(
+        url, headers={"Cookie": f"session={os.environ['COOKIE_SESSION']}"}
+    )
     with open(f"day{day}/input.txt", "w") as file:
         file.write(response.text)
 
@@ -26,10 +29,11 @@ def copy_template(directory: str):
 def main(day: str):
     directory = f"day{day}"
     create_directory_for_day(directory)
-    # download_input(day)
+    download_input(day)
     copy_template(directory)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     args = sys.argv
     main(args[1])
